b1a6339481bbb97bca9047f4a9644fbe2aa1ccfe
jellopy <parnupat.j@gmail.com>
2023-07-04 08:02:25 +0700

    init project

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '@vue-flow/core/dist/style.css';
+import '@vue-flow/core/dist/theme-default.css';
+import './assets/normalize.css';
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
