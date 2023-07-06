3b591e43d660d446ca873a59a1b32ee156b702b3
jellopy <parnupat.j@gmail.com>
2023-07-06 23:29:39 +0700

    drag and drop flow

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,79 @@
+<script setup>
+import { VueFlow, useVueFlow } from '@vue-flow/core'
+import { nextTick, watch } from 'vue'
+import Sidebar from './components/Sidebar.vue'
+import { MiniMap } from '@vue-flow/minimap'
+import { Background } from '@vue-flow/background'
+
+let id = 0
+function getId() {
+  return `dndnode_${id++}`
+}
+
+const { findNode, onConnect, addEdges, addNodes, project, vueFlowRef } = useVueFlow({
+  nodes: [
+    {
+      id: '1',
+      type: 'input',
+      label: 'input node',
+      position: { x: 250, y: 25 },
+    },
+  ],
+})
+
+function onDragOver(event) {
+  event.preventDefault()
+
+  if (event.dataTransfer) {
+    event.dataTransfer.dropEffect = 'move'
+  }
+}
+
+onConnect((params) => addEdges(params))
+
+function onDrop(event) {
+  const type = event.dataTransfer?.getData('application/vueflow')
+
+  const { left, top } = vueFlowRef.value.getBoundingClientRect()
+
+  const position = project({
+    x: event.clientX - left,
+    y: event.clientY - top,
+  })
+
+  const newNode = {
+    id: getId(),
+    type,
+    position,
+    label: `${type} node`,
+  }
+
+  addNodes([newNode])
+
+  // align node position after drop, so it's centered to the mouse
+  nextTick(() => {
+    const node = findNode(newNode.id)
+    const stop = watch(
+      () => node.dimensions,
+      (dimensions) => {
+        if (dimensions.width > 0 && dimensions.height > 0) {
+          node.position = { x: node.position.x - node.dimensions.width / 2, y: node.position.y - node.dimensions.height / 2 }
+          stop()
+        }
+      },
+      { deep: true, flush: 'post' },
+    )
+  })
+}
+</script>
+
 <template>
-  <div :style="{
-      height: '100vh',
-      width: '100vw'
-    }">
-    <VueFlow class="basicflow" v-model="elements" >
+  <div class="dndflow" @drop="onDrop">
+    <VueFlow @dragover="onDragOver" :default-viewport="{ zoom: 1.5 }" :min-zoom="0.2" :max-zoom="4">
       <Background pattern-color="#aaa" gap="8" />
-      <MiniMap />
     </VueFlow>
+    <!-- <Background pattern-color="#aaa" gap="8" /> -->
+      <MiniMap />
+    <Sidebar />
   </div>
 </template>
-<script setup>
-  import { ref } from 'vue'
-  import { VueFlow  } from '@vue-flow/core'
-  import { MiniMap } from '@vue-flow/minimap'
-  import { Background } from '@vue-flow/background'
-  const elements = ref([
-  // Nodes
-  // An input node, specified by using `type: 'input'`
-  { id: '1', type: 'input', label: 'Node 1', position: { x: 250, y: 5 } },
-
-  // Default nodes, you can omit `type: 'default'`
-  { id: '2', label: 'Node 2', position: { x: 100, y: 100 }, },
-  { id: '3', label: 'Node 3', position: { x: 400, y: 100 } },
-
-  // An output node, specified by using `type: 'output'`
-  { id: '4', type: 'output', label: 'Node 4', position: { x: 400, y: 200 } },
-
-  // Edges
-  // Most basic edge, only consists of an id, source-id and target-id
-  { id: 'e1-3', source: '1', target: '3' },
-
-  // An animated edge
-  { id: 'e1-2', source: '1', target: '2', animated: true },
-])
-</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import '@vue-flow/core/dist/style.css';
 import '@vue-flow/core/dist/theme-default.css';
 import './assets/normalize.css';
+import './assets/style.css';
 import { createApp } from 'vue'
 import App from './App.vue'
 
